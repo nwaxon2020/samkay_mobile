2c7949472b4754c1a6bd01db56c9ff65239e20bc
nwaxon2020 <princenwachukwu308@yahoo.com>
2026-01-12 10:17:12 +0100

    padding bottom

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -134,7 +134,7 @@ export default function Hero() {
             </div>
         )}
         
-        <div className="container mx-auto px-4 md:px-8 md:py-16 relative z-10">
+        <div className="container mx-auto px-4 md:px-8 pb-8 md:py-16 relative z-10">
             <div className="max-w-7xl mx-auto">
             
             {/* Mobile Layout (Stacked) */}

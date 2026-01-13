d56d20da53d96368822c3caa5081dd169a247162
nwaxon2020 <princenwachukwu308@yahoo.com>
2026-01-13 22:01:15 +0100

    purchase about to start

---
+++ app/purchase/page.tsx
@@ -0,0 +1,7 @@
+export default function Purchase(){
+    return(
+        <div className="w-full p-8 bg-black h-[90vh]">
+            <h1 className="mt-30 font-black text-white text-3xl md:text-4xl">We Build the Purchase Payment Logic Here!</h1>
+        </div>
+    )
+}
